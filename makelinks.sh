34a177d231a2b8e4848f207d554a97cb5706afe0
rafael@taurus <rafael.luz@protonmail.com>
2022-02-27 03:14:09 -0300

    correct error in makelink.sh

--- makelinks.sh
+++ makelinks.sh
@@ -9,30 +9,28 @@ DOTFILES=(
 function create_symbolic_link() {
 
   if [ -L "$HOME/$1" ]; then
-
       echo "$HOME/$1 is already a link. Nothing will be done"
 
-  elif [ -f "$HOME/$1" ]; then
+  else
+    if [ -f "$HOME/$1" ]; then
 
-    echo "$HOME/$1 already exists. Removing it..."
-    rm "$HOME/$1"
+      echo "$HOME/$1 already exists. Removing it..."
+      rm "$HOME/$1"
+
+    elif [ -d "$HOME/$1" ]; then
+
+      for content in "$HOME/$1"/*; do
+        echo "Moving $content to $HOME/dotfiles/$1"
+        mv -vn "$content" "$HOME/dotfiles/$1"
+      done
+
+      echo "Removing $HOME/$1"
+      rm -rf "$HOME/$1"
+    fi
 
     echo "Creating link to $HOME"
-  	ln -s "$HOME/dotfiles/$1" "/$HOME/$1"
-
-	elif [ -d "$HOME/$1" ]; then
-
-		for content in "$HOME/$1"/*; do
-			echo "Moving $content to $HOME/dotfiles/$1"
-			mv -vn "$content" "$HOME/dotfiles/$1"
-		done
-
-		echo "Removing $HOME/$1"
-		rm -rf "$HOME/$1"
-
-    echo "Creating link to $HOME"
-  	ln -s "$HOME/dotfiles/$1" "/$HOME/$1"
-	fi
+    ln -s "$HOME/dotfiles/$1" "/$HOME/$1"
+  fi
 }
 
 
