4fa9f32987e3c779fced06df77cc36bccfa7d396
rafael@divenger <rafael.luz@protonmail.com>
2022-09-20 10:33:19 -0300

    modify lua in divenger

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,5 @@
-<<<<<<< HEAD
-vim.opt_local.path:prepend(vim.fn.stdpath("config") .. "/lua")
-
-require("options")
-
-require("packer-setup")
--- require("nvim-tree-setup")
--- require("telescope-setup")
-require("colorscheme")
-
--- require("mappings")
--- require("completions-setup")
--- require("lsp-setup")
--- require("treesitter-setup")
--- require("autopairs-setup")
--- require("neoclip-setup")
--- require("vimwiki-setup")
--- require("dap-setup")
--- require("luasnip-setup")
-=======
 require("options")
 require("mappings")
 require("packer-config")
 require("nvim-tree-config")
 require("colourschemes")
->>>>>>> 7457a201094317b9961b6daf0f8f9f9f40caa64f
